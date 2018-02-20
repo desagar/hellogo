e12352600d09763c97b756947b44370ff351be68
Deva Sagar <deva.sagar@oracle.com>
2018-02-20 11:56:15 -0500

    Go Lang hello

---
+++ hello.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+import "os"
+
+//comment
+func main() {
+    var x string = "Hello, I am Shadow"
+    
+    //declare constant and infer type from RHS without type specified
+    const y = "Bringer of darkness"
+
+    //declare, initialize and infer type from RHS with ':=' assignment
+    l := len(x)
+    x += "\n" + y
+    fmt.Println(x)
+    fmt.Println(l)
+    os.Exit(0)
+}
